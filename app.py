8c83f56833d8af74d9dce08e989b4fe91a7d278b
bdoey <115423679+bdoey@users.noreply.github.com>
2023-02-27 15:33:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ st.table(features_df)
 
 if st.button('Predict'):
         result = predict(np.array([loan_amnt, term, emp_length, annual_inc, dti, delinq_2yrs, open_acc, mort_acc, pub_rec_bankruptcies, last_fico_range_high]))
-        st.text(prediction[0])
+        st.text(result[0])
